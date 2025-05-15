2fc3fb476fa6d63c2675ac37a05d110956aa4c0d
James Davies <jimdavies72@googlemail.com>
2025-05-15 08:53:08 +0100

    added head tag to layout.ts for google analytics scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import "./globals.css";
+import Script from "next/script";
 
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 
-import { DM_Sans } from 'next/font/google'
+import { DM_Sans } from "next/font/google";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -14,14 +15,29 @@ const dmSans = DM_Sans({
   variable: "--font-dm-sans",
 });
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={dmSans.variable} >
+    <html lang="en" className={dmSans.variable}>
+      <head>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+        />
+        <Script id="ga-script" strategy="lazyOnload">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+      </head>
       <body className="bg-[#070815] text-white">
         <Header />
         <main>{children}</main>
